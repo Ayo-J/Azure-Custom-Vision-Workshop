85ec69a85332195d85e17a0ff2eee2f50589abba
Ayo <118840840+Ayo-J@users.noreply.github.com>
2024-07-22 18:20:20 +0530

    Add files via upload

---
+++ Custom-Vision/image classification/Url.ps1
@@ -0,0 +1,30 @@
+# Set the prediction URL and key
+$predictionUrl = "image"
+$predictionKey = "header"
+
+# Prompt the user to select an image file
+#$imagePath = ".\Picture30.jpg"
+
+$img_num = 3
+if ($args.count -gt 0 -And $args[0] -in (1..3))
+{
+    $img_num = $args[0]
+}
+
+$img = "https://raw.githubusercontent.com/MicrosoftLearning/AI-900-AIFundamentals/main/data/vision/animals/animal-$($img_num).jpg"
+
+$headers = @{}
+$headers.Add( "Prediction-Key", $predictionKey )
+$headers.Add( "Content-Type","application/json" )
+
+$body = "{'url' : '$img'}"
+
+write-host "Analyzing image..."
+$result = Invoke-RestMethod -Method Post `
+          -Uri $predictionUrl `
+          -Headers $headers `
+          -Body $body | ConvertTo-Json -Depth 5
+
+$prediction = $result | ConvertFrom-Json
+
+Write-Host ("`n",$prediction.predictions[0].tagName, "`n")
